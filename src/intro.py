d2995b54c8619f609f55ab3e07dc630a9425ac2e
Polycarp Anyanwu <polyanyanwu@gmail.com>
2022-01-14 19:35:26 +0000

    feat: Add introduction logo to intro.py

--- src/intro.py
+++ src/intro.py
@@ -1,17 +1,33 @@
 """print the initial welcome to user and request username"""
 
 
-from src.color_prints import printCyan, printBlue, printRed
+from src.color_prints import printCyan, printBlue, printRed, printWhite
 from src.username import Username
+from time import sleep
 
 
 def introduction():
     """ print initial welcome message and obtain username for the user"""
 
+    print_intro_logo()
+    sleep(3)
     printCyan('Welcome Dear Pension Investment Enthusiast!\n')
     print('Do you operate a Retirement Savings Account in Nigeria? Or')
-    print('Are you an interested Nigerian that wants to know more about')
-    print('   returns on investments of pension assets?')
+    print('Are you an interested person that wants to know more about')
+    print('   returns on investments of pension assets in Nigeria?')
     printCyan("Let’s find out which PFAs achieve best returns on pension funds.\n")
     user_name = Username()
     return user_name.username
+
+def print_intro_logo():
+    """ print the introdution logo """
+
+    printCyan("OOOOOOOOO     OOOOOOOOO    OO         OO           OO")
+    printWhite("OO     OO     OO           OO OO      OO          OO OO")
+    printCyan("OO     OO     OO           OO  OO     OO         OO   OO")
+    printWhite("OOOOOOOOO     OOOOOOOOO    OO   OO    OO        OO     OO")
+    printCyan("OO            OO           OO    OO   OO  OO   OO OOOOO OO  OO")
+    printWhite("OO            OO           OO     OO  OO      OO         OO ")
+    printCyan("OO	       OO           OO      OO OO     OO           OO")
+    printWhite("OO	       OOOOOOOOO    OO         OO    OO             OO\n")
+    printWhite("THE NIGERIAN PENSION ADVISER\n")
